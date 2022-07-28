c2f3b898c38f8b7916c4b08c9889c9069cfaa957
Anthony <anthonylys@outlook.com>
2022-07-29 01:35:27 +0800

    add orderbook; init simple strat

---
+++ livebot/data_structure/orderbook.py
@@ -0,0 +1,37 @@
+import asyncio
+
+class OrderBook:
+    def __init__(self, keywords, logger=None):
+        self.keywords = keywords
+        self.orderbook = {}
+        self.logger = logger
+        
+    async def clear(self):
+        self.orderbook = {key: -1 for key in self.keywords}
+        if self.logger:
+            self.logger.info("Orderbook cleared")
+    
+    async def update(self, key, value):
+        if key in self.orderbook:
+            self.orderbook[key] = value
+            if self.logger:
+                self.logger.info("Orderbook updated: {} = {}".format(key, value))
+        else:
+            if self.logger:
+                self.logger.error("Orderbook update failed: {} not in orderbook".format(key))
+    
+    async def updates(self, orderbook):
+        for key, value in orderbook.items():
+            await self.update(key, value)
+    
+    async def get(self, key):
+        if key in self.orderbook:
+            return self.orderbook[key]
+        else:
+            if self.logger:
+                self.logger.error("Orderbook get failed: {} not in orderbook".format(key))
+            return -1
+    
+    async def gets(self):
+        return self.orderbook
+        
