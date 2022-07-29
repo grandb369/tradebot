0402ef513d7301835d6e18c43e3a73698c79969b
Anthony <anthonylys@outlook.com>
2022-07-29 17:51:36 +0800

    add sl and tp order

--- livebot/simple_strat.py
+++ livebot/simple_strat.py
@@ -1,3 +1,4 @@
+from cmath import log
 from binance.client import Client
 from binance import BinanceSocketManager
 from sympy import re
@@ -32,6 +33,7 @@ async def execute_order(
     price,
     precision,
     params,
+    logger,
     is_stop_loss_order=False,
     is_take_profit_order=False,
     pre_order_id=-1
@@ -56,26 +58,93 @@ async def execute_order(
             stoploss_orders[order_id] = pre_order_id
             stoploss_orders[pre_order_id] = order_id
             await params.update_param("stoploss_orders", stoploss_orders)
+            await logger.info("Stoploss order - price: {}, amount: {}, id: {}, pre_order_id: {}".format(price, amount, order_id, pre_order_id))")
         elif is_take_profit_order:
-            take_profit_orders[order_id] = pre_order_id
-            take_profit_orders[pre_order_id] = order_id
+            take_profit_orders[order_id]=pre_order_id
+            take_profit_orders[pre_order_id]=order_id
             await params.update_param("take_profit_orders", take_profit_orders)
+            await logger.info("Take profit order - price: {}, amount: {}, id: {}, pre_order_id: {}".format(price, amount, order_id, pre_order_id))
         else:
             regular_orders.add(order_id)
             await params.update_param("regular_orders", regular_orders)
+            await logger.info("Regular order - price: {}, amount: {}, id: {}".format(price, amount, order_id))
 
+
+async def send_take_profit_order(client, symbol, logger, err, orderbook, params, order_id, side, filled_qty, fill_price):
+    tp_price=fill_price
+    precision=await orderbook.get('precision')
+    if side.upper() == BUY:
+        tp_price *= 1.1
+        await execute_order(
+            client = client,
+            symbol = symbol,
+            side = SELL,
+            order_type = LIMIT,
+            amount = filled_qty,
+            price = tp_price,
+            precision = precision,
+            params = params,
+            is_take_profit_order = True,
+            pre_order_id = order_id
+        )
+    else:
+        tp_price *= 0.9
+        await execute_order(
+            client = client,
+            symbol = symbol,
+            side = BUY,
+            order_type = LIMIT,
+            amount = filled_qty,
+            price = tp_price,
+            precision = precision,
+            params = params,
+            is_take_profit_order = True,
+            pre_order_id = order_id
+        )
+
+async def send_stop_loss_order(client, symbol, logger, err, orderbook, params, order_id, side, filled_qty, fill_price):
+    sl_price=fill_price
+    precision=await orderbook.get('precision')
+    if side.upper() == BUY:
+        sl_price *= 0.9
+        await execute_order(
+            client = client,
+            symbol = symbol,
+            side = SELL,
+            order_type = LIMIT,
+            amount = filled_qty,
+            price = sl_price,
+            precision = precision,
+            params = params,
+            is_stop_loss_order = True,
+            pre_order_id = order_id
+        )
+    else:
+        sl_price *= 1.1
+        await execute_order(
+            client = client,
+            symbol = symbol,
+            side = BUY,
+            order_type = LIMIT,
+            amount = filled_qty,
+            price = sl_price,
+            precision = precision,
+            params = params,
+            is_stop_loss_order = True,
+            pre_order_id = order_id
+        )
 
 async def order_filled_socket(client, symbol, logger, err, orderbook, params):
-    bsm = BinanceSocketManager(client)
-    timer = time.time()
-    keep_alive = 60*50
+    bsm=BinanceSocketManager(client)
+    timer=time.time()
+    keep_alive=60*50
     async with bsm.user_socket() as stream:
         while True:
             # Keep socket alive
             if time.time() - timer > keep_alive:
-                keep_alive = time.time()
-                listen_key = await stream.get_listen_key()
-                res = await client.keep_alive(listen_key)
+                keep_alive=time.time()
+                listen_key=await stream.get_listen_key()
+                res=await client.keep_alive(listen_key)
                 logger.info(
                     "Keep alive: {}, Listen Key: {}".format(res, listen_key))
             if err.status:
@@ -84,22 +153,22 @@ async def order_filled_socket(client, symbol, logger, err, orderbook, params):
                 await logger.err("order_filled_socket: socket closed")
                 break
             try:
-                res = await stream.recv()
+                res=await stream.recv()
             except Exception as e:
                 await logger.err("order_filled_socket: {}".format(e))
                 continue
             else:
                 if res['e'] == "ORDER_TRADE_UPDATE":
-                    order = res['o']
+                    order=res['o']
                     if order['s'] == symbol and order['x'] == 'FILLED':
-                        regular_orders = await params.get_param("regular_orders")
-                        take_profit_orders = await params.get_param("take_profit_orders")
-                        stoploss_orders = await params.get_param("stoploss_orders")
+                        regular_orders=await params.get_param("regular_orders")
+                        take_profit_orders=await params.get_param("take_profit_orders")
+                        stoploss_orders=await params.get_param("stoploss_orders")
 
-                        order_id = order['i']
-                        side = order['S']
-                        filled_qty = order['l']
-                        fill_price = float(order['L'])
+                        order_id=order['i']
+                        side=order['S'].lower()
+                        filled_qty=order['l']
+                        fill_price=float(order['L'])
 
                         if order_id in regular_orders:
                             regular_orders.remove(order_id)
@@ -110,16 +179,16 @@ async def order_filled_socket(client, symbol, logger, err, orderbook, params):
                             await send_stop_loss_order(client, symbol, logger, err, orderbook, params, order_id, side, filled_qty, fill_price)
                         elif order in take_profit_orders:
                             await logger.info("Take Profit Order Filled: id: {}, side: {}, filled_qty: {}, fill_price: {}".format(order_id, side, filled_qty, fill_price))
-                            prev_id = take_profit_orders[order_id]
-                            st_id = stoploss_orders[prev_id]
+                            prev_id=take_profit_orders[order_id]
+                            st_id=stoploss_orders[prev_id]
                             del take_profit_orders[order_id]
                             del take_profit_orders[prev_id]
 
                             await cancel_stoploss_order(client, symbol, logger, err, orderbook, params, st_id)
                         elif order in stoploss_orders:
                             await logger.info("Stop Loss Order Filled: id: {}, side: {}, filled_qty: {}, fill_price: {}".format(order_id, side, filled_qty, fill_price))
-                            prev_id = stoploss_orders[order_id]
-                            tp_id = take_profit_orders[prev_id]
+                            prev_id=stoploss_orders[order_id]
+                            tp_id=take_profit_orders[prev_id]
                             del stoploss_orders[order_id]
                             del stoploss_orders[prev_id]
 
@@ -129,11 +198,11 @@ async def order_filled_socket(client, symbol, logger, err, orderbook, params):
                             print("Unknow Order Filled: id: {}, side: {}, filled_qty: {}, fill_price: {}".format(
                                 order_id, side, filled_qty, fill_price))
                 elif res['e'] == 'ACCOUNT_UPDATE':
-                    position = res['a']['P']
+                    position=res['a']['P']
                     for pos in position:
                         if pos['s'] == symbol and pos['ps'] == "BOTH":
-                            posAmt = pos['pa']
-                            pnl = pos['up']
+                            posAmt=pos['pa']
+                            pnl=pos['up']
                             await params.update_param("posAmt", posAmt)
                             await params.update_param("pnl", pnl)
         logger.info("order_filled_socket: socket closed")
@@ -141,15 +210,15 @@ async def order_filled_socket(client, symbol, logger, err, orderbook, params):
 
 async def regular_order_stream(client, symbol, logger, err, orderbook, params):
     await logger.info("regular_order_stream: started")
-    order_interval = await params.get_param("order_interval")
-    order_interval = int(order_interval)
-    max_order = await params.get_param("max_order")
-    enable_close_position = await params.get_param("close_position")
-    enable_close_open_orders = await params.get_param("close_open_orders")
+    order_interval=await params.get_param("order_interval")
+    order_interval=int(order_interval)
+    max_order=await params.get_param("max_order")
+    enable_close_position=await params.get_param("close_position")
+    enable_close_open_orders=await params.get_param("close_open_orders")
 
     await logger.info("Regular Order Interval: {}".format(order_interval))
     await asyncio.sleep(5)
-    timer = time.time()
+    timer=time.time()
     while True:
         if err.status:
             await logger.err("regular_order_stream: terminated")
@@ -160,10 +229,10 @@ async def regular_order_stream(client, symbol, logger, err, orderbook, params):
             break
         if time.time() - timer > order_interval or len(await params.get_param("regular_orders")) < max_order:
             await cancel_all_unfilled_orders(client, symbol, logger, err, orderbook, params)
-            best_bid = await orderbook.get('bid')
-            best_ask = await orderbook.get('ask')
-            amount = await orderbook.get("amount")
-            precision = await orderbook.get('precision')
+            best_bid=await orderbook.get('bid')
+            best_ask=await orderbook.get('ask')
+            amount=await orderbook.get("amount")
+            precision=await orderbook.get('precision')
 
             # calculate bid/ask depending on the other indicators
             # end
@@ -178,6 +247,7 @@ async def regular_order_stream(client, symbol, logger, err, orderbook, params):
                 amount=amount,
                 price=best_bid,
                 precision=precision,
+                logger=logger
             )
             # ASK
             await execute_order(
@@ -188,6 +258,7 @@ async def regular_order_stream(client, symbol, logger, err, orderbook, params):
                 amount=amount,
                 price=best_ask,
                 precision=precision,
+                logger=logger
             )
             timer = time.time()
         await asyncio.sleep(1)
